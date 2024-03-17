c8ff38de0be8aa9c849397882914d5daf4ef8485
KilliannH <killiannhervagault@gmail.com>
2024-03-17 17:23:01 +0100

    modifs for the pi

--- subprocess.js
+++ subprocess.js
@@ -1,8 +1,5 @@
 import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import 'dotenv/config'
-
-puppeteer.use(StealthPlugin());
 
 const twitchBaseUrl = process.env.TWITCH_BASE_URL;
 const channelName = process.env.TWITCH_CHANNEL;
@@ -10,17 +7,12 @@ const channelName = process.env.TWITCH_CHANNEL;
 const goToTwichPage = () => {
   // Launch the browser and open a new blank page
     return new Promise((resolve, reject) => {
-      puppeteer.launch({headless: true}).then((browser) => {
+      puppeteer.launch({headless: false}).then((browser) => {
       browser.newPage().then((page) => {
       // Navigate the page to a URL
       page.goto(process.env.PROXY_URL).then(() => {
         page.setViewport({width: 1080, height: 1024}).then(() => {
           page.type('.typeahead__query', twitchBaseUrl + channelName).then(() => {
-          const cookieConsentSelector = '#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll';
-
-            page.waitForSelector(cookieConsentSelector).then(() => {
-              page.click(cookieConsentSelector).then(() => {
-              // Wait and click on first result
                 const searchResultSelector = '#requestSubmit';
                 page.waitForSelector(searchResultSelector).then(() => {
                     page.click(searchResultSelector).then(() => {
@@ -42,8 +34,6 @@ const goToTwichPage = () => {
           });
         });
       });
-    });
-    });
   }).catch((e) => reject({processFinishedWithoutErrors: false, error: e}));
 }
 
